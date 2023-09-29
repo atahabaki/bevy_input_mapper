c262b305f9918ef14506ae406f3ffedfc135e9f9
A. Taha Baki <atahabaki@pm.me>
2023-09-29 14:18:03 +0300

    rfct: update bind_keys function with the modern way

--- examples/binding_with_code.rs
+++ examples/binding_with_code.rs
@@ -1,35 +1,24 @@
 use bevy::prelude::*;
 use bevy_input_mapper::{
     input::{events::*, gamepad::GamepadAxis, mouse::MouseAxis},
-    AutoBinder, InputMapper, InputMapperPlugin,
+    InputMapper, InputMapperPlugin,
 };
 
 fn bind_keys(mut im: ResMut<InputMapper>) {
-    im.keyboard_binding.bind(KeyCode::Space, "jump".to_string());
-    im.mouse_button_binding
-        .bind(MouseButton::Left, "fire".to_string());
-    im.mouse_axis_binding
-        .bind(MouseAxis::PositiveX, "look_right".to_string());
-    im.mouse_axis_binding
-        .bind(MouseAxis::NegativeY, "look_up".to_string());
-    im.mouse_axis_binding
-        .bind(MouseAxis::NegativeX, "look_left".to_string());
-    im.mouse_axis_binding
-        .bind(MouseAxis::PositiveY, "look_down".to_string());
-    im.gamepad_axis_binding
-        .bind(GamepadAxis::PositiveRightStickY, "look_up".to_string());
-    im.gamepad_axis_binding
-        .bind(GamepadAxis::NegativeRightStickY, "look_down".to_string());
-    im.gamepad_axis_binding
-        .bind(GamepadAxis::PositiveRightStickX, "look_right".to_string());
-    im.gamepad_axis_binding
-        .bind(GamepadAxis::NegativeRightStickX, "look_left".to_string());
-    im.gamepad_button_binding
-        .bind(GamepadButtonType::LeftTrigger, "scope".to_string());
-    im.gamepad_button_binding
-        .bind(GamepadButtonType::RightTrigger2, "fire".to_string());
-    im.gamepad_button_binding
-        .bind(GamepadButtonType::South, "jump".to_string());
+    im.bind_keyboard_key_press(KeyCode::Space, "jump")
+        .bind_gamepad_button_press(GamepadButtonType::South, "jump")
+        .bind_mouse_axis_move(MouseAxis::PositiveX, "look_right")
+        .bind_mouse_axis_move(MouseAxis::NegativeX, "look_left")
+        .bind_mouse_axis_move(MouseAxis::PositiveY, "look_down")
+        .bind_mouse_axis_move(MouseAxis::NegativeY, "look_up")
+        .bind_gamepad_axis_move(GamepadAxis::PositiveRightStickX, "look_right")
+        .bind_gamepad_axis_move(GamepadAxis::NegativeRightStickX, "look_left")
+        .bind_gamepad_axis_move(GamepadAxis::NegativeRightStickY, "look_down")
+        .bind_gamepad_axis_move(GamepadAxis::PositiveRightStickY, "look_up")
+        .bind_mouse_button_press(MouseButton::Right, "scope")
+        .bind_gamepad_button_press(GamepadButtonType::LeftTrigger, "scope")
+        .bind_mouse_button_press(MouseButton::Left, "fire")
+        .bind_gamepad_button_press(GamepadButtonType::RightTrigger2, "fire");
 }
 
 fn logger(
